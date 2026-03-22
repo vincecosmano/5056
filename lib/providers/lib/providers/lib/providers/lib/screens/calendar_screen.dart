83fa8052bed54c48a9b2f055b2cbcee53a0629c7
vincecosmano <vincecosmano@gmail.com>
2026-03-23 08:14:25 +1100

    Create CalendarScreen with basic layout
    Initial implementation of CalendarScreen with basic UI.

---
+++ lib/providers/lib/providers/lib/providers/lib/screens/calendar_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CalendarScreen extends StatefulWidget {
+  @override
+  _CalendarScreenState createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calendar'),
+      ),
+      body: Center(
+        child: Text('Calendar view will be displayed here'),
+      ),
+    );
+  }
+}
