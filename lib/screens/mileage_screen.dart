f23c04412a05e51a44b6c66440d2d707ee4834fe
vincecosmano <vincecosmano@gmail.com>
2026-03-22 11:41:09 +1100

    Create MileageScreen file

---
+++ lib/screens/mileage_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MileageScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mileage'),
+      ),
+      body: Center(
+        child: Text('Mileage Screen'),
+      ),
+    );
+  }
+}
