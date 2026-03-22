ca2d54fe021414aa973b94d7cb4de5b019d1c923
vincecosmano <vincecosmano@gmail.com>
2026-03-22 21:00:12 +1100

    Create InvoicesScreen widget

---
+++ lib/screens/invoices_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InvoicesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Invoices'),
+      ),
+      body: Center(
+        child: Text('Invoices will be displayed here'),
+      ),
+    );
+  }
+}
